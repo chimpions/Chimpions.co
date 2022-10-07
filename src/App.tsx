f6ad4af21ba5b6b0289cfa4ffda68e4c124737e7
minne-ape-olis <minne.ape.olis@gmail.com>
2022-10-07 10:37:07 -0500

    added merch redemption list

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Navigation from '_components/Navigation/Navigation';
-import Landing from '_components/Landing/Landing';
-import Compendium from '_components/Compendium/Compendium';
-import OurVision from '_components/OurVision/OurVision';
-import TheChiao from '_components/TheChiao/TheChiao';
-// Import the functions you need from the SDKs you need
+import Navigation from "_components/Navigation/Navigation";
+import Landing from "_components/Landing/Landing";
+import Compendium from "_components/Compendium/Compendium";
+import OurVision from "_components/OurVision/OurVision";
+import TheChiao from "_components/TheChiao/TheChiao";
+import MerchRedemption from "_components/MerchRedemption/MerchRedemption";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import TogetherWeStand from '_components/TogetherWeStand/TogetherWeStand';
-import Artist from '_components/TogetherWeStand/Artist';
-import { Drop, drops } from '_content/tws';
+import TogetherWeStand from "_components/TogetherWeStand/TogetherWeStand";
+import Artist from "_components/TogetherWeStand/Artist";
+import { Drop, drops } from "_content/tws";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -24,29 +24,32 @@ const firebaseConfig = {
   storageBucket: "chimpions-web.appspot.com",
   messagingSenderId: "415236253475",
   appId: "1:415236253475:web:fc262073dcdad293c00e96",
-  measurementId: "G-Q65VSKGTC3"
+  measurementId: "G-Q65VSKGTC3",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-
 export default function ChimpionsWeb() {
-	return (
-		<Router>
-			<Navigation />
-			<Routes>
-				<Route path='/' element={<Landing />} />
+  return (
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Landing />} />
 
-				<Route path='/compendium' element={<Compendium />} />
-				<Route path='/our-vision' element={<OurVision />} />
-				<Route path='/the-chiao' element={<TheChiao />} />
-				<Route path='/treehouse' element={<TogetherWeStand />} />
-				{drops.map((Drop) => (
-					<Route path={`/treehouse/${Drop.artistPath}`} element={<Artist drop={Drop}/>} />
-				))}
-			</Routes>
-		</Router>
-	);
+        <Route path="/compendium" element={<Compendium />} />
+        <Route path="/our-vision" element={<OurVision />} />
+        <Route path="/the-chiao" element={<TheChiao />} />
+        <Route path="/treehouse" element={<TogetherWeStand />} />
+        <Route path="/merchRedemption" element={<MerchRedemption />} />
+        {drops.map((Drop) => (
+          <Route
+            path={`/treehouse/${Drop.artistPath}`}
+            element={<Artist drop={Drop} />}
+          />
+        ))}
+      </Routes>
+    </Router>
+  );
 }
--- src/_components/Navigation/Navigation.tsx
+++ src/_components/Navigation/Navigation.tsx
@@ -1,43 +1,49 @@
-import React from 'react';
+import React from "react";
 
 export default function Navigation() {
-	return (
-		<nav className='fixed top-0 left-0 w-full z-20 bg-opacity-25 border-b border-white border-opacity-10 backdrop-blur-md'>
-			<div className='w-full max-w-screen-lg mx-auto flex flex-row justify-between items-center px-4 py-3 md:py-6'>
-				<a href='/' className='block cursor-pointer hover:opacity-70'>
-					<img
-						src='/images/logos/Logo_Base.png'
-						className='h-5 md:h-8'
-						alt='Chimpions Navigation Logo'
-					/>
-				</a>
-				<div className='font-display flex items-center gap-x-4'>
-					<a
-						href='/treehouse'
-						className='p-2 hover:rounded-md hover:text-pink-500 hover:bg-pink-500 hover:bg-opacity-10'
-					>
-						TreeHouse
-					</a>
-					<a
-						href='/compendium'
-						className='p-2 hover:rounded-md hover:text-pink-500 hover:bg-pink-500 hover:bg-opacity-10'
-					>
-						Compendium
-					</a>
-					<a
-						href='/our-vision'
-						className='p-2 hover:rounded-md hover:text-pink-500 hover:bg-pink-500 hover:bg-opacity-10'
-					>
-						Our Vision
-					</a>
-					<a
-						href='/the-chiao'
-						className='p-2 hover:rounded-md hover:text-pink-500 hover:bg-pink-500 hover:bg-opacity-10'
-					>
-						The Chiao
-					</a>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="fixed top-0 left-0 w-full z-20 bg-opacity-25 border-b border-white border-opacity-10 backdrop-blur-md">
+      <div className="w-full max-w-screen-lg mx-auto flex flex-row justify-between items-center px-4 py-3 md:py-6">
+        <a href="/" className="block cursor-pointer hover:opacity-70">
+          <img
+            src="/images/logos/Logo_Base.png"
+            className="h-5 md:h-8"
+            alt="Chimpions Navigation Logo"
+          />
+        </a>
+        <div className="font-display flex items-center gap-x-4">
+          <a
+            href="/treehouse"
+            className="p-2 hover:rounded-md hover:text-pink-500 hover:bg-pink-500 hover:bg-opacity-10"
+          >
+            TreeHouse
+          </a>
+          <a
+            href="/compendium"
+            className="p-2 hover:rounded-md hover:text-pink-500 hover:bg-pink-500 hover:bg-opacity-10"
+          >
+            Compendium
+          </a>
+          <a
+            href="/our-vision"
+            className="p-2 hover:rounded-md hover:text-pink-500 hover:bg-pink-500 hover:bg-opacity-10"
+          >
+            Our Vision
+          </a>
+          <a
+            href="/the-chiao"
+            className="p-2 hover:rounded-md hover:text-pink-500 hover:bg-pink-500 hover:bg-opacity-10"
+          >
+            The Chiao
+          </a>
+          <a
+            href="/merchRedemption"
+            className="p-2 hover:rounded-md hover:text-pink-500 hover:bg-pink-500 hover:bg-opacity-10"
+          >
+            Merch Redemption List
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 }
